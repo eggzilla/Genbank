d912b32eafcde54303c030fed25e6e14d4aeae6b
Florian Eggenhofer <egg@tbi.univie.ac.at>
2014-05-20 11:51:08 +0200

    Added testing script for genbank parser

---
+++ GenbankParserTest.hs
@@ -0,0 +1,27 @@
+-- | Parser test script
+--   read from file and directly print parsing output
+
+module Main where
+    
+import System.Environment (getArgs)
+import System.Process 
+import Text.ParserCombinators.Parsec
+import System.IO
+import System.Environment
+import Data.List
+import Bio.GenbankParser
+import System.Directory
+import System.Cmd
+import Control.Monad    
+import Data.Either
+import Data.Either.Unwrap
+    
+main = do
+  args <- getArgs
+  let input_file = (head args)
+  let output_file = (last args)
+                                      
+  -- read Clustal outputfile
+  --input_file_content <- readFile input_file                       
+  parsedinput <- readGenbank input_file 
+  print parsedinput
