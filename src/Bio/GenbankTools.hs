79dbed382f8addd97d1b10611c21e4f9d2e87001
Florian Eggenhofer <egg@tbi.univie.ac.at>
2014-06-05 13:00:28 +0200

    Added function to extract sequences for specific features

--- src/Bio/GenbankTools.hs
+++ src/Bio/GenbankTools.hs
@@ -1,6 +1,7 @@
 -- | Parse Genebank format
 module Bio.GenbankTools (
-                       extractFeatureSequences,
+                       extractAllFeatureSequences,
+                       extractSpecificFeatureSequences,
                        extractSubSequence,
                        module Bio.GenbankData
                       ) where
@@ -13,10 +14,17 @@ import Bio.Core.Sequence
 import Data.Int
 import qualified Data.ByteString.Lazy.Char8 as L
 
--- | Parse the input as Genbank datatype
-extractFeatureSequences :: Genbank -> [SeqData]
-extractFeatureSequences genbank = sequences
+-- |
+extractAllFeatureSequences :: Genbank -> [SeqData]
+extractAllFeatureSequences genbank = sequences
   where coordinates = map featureCoordinates (features genbank)
+        fullSequence = origin genbank
+        sequences = map (extractSubSequence fullSequence) coordinates
+
+-- |
+extractSpecificFeatureSequences :: String -> Genbank -> [SeqData]
+extractSpecificFeatureSequences specificFeature genbank = sequences
+  where coordinates = map featureCoordinates (filter (\x -> ((featureType x) == (L.pack specificFeature)))(features genbank))
         fullSequence = origin genbank
         sequences = map (extractSubSequence fullSequence) coordinates
         
@@ -26,9 +34,11 @@ extractSubSequence genbankSeq seqCoordinates
   | otherwise = SeqData subsequence
   where endTruncatedSequence = L.take (fromIntegral ((coordinatesFrom seqCoordinates) + (coordinatesTo seqCoordinates)):: Int64) (unSD genbankSeq)
         subsequence = L.drop (fromIntegral (coordinatesFrom seqCoordinates) :: Int64) endTruncatedSequence
-        
--- | Calculate the reverse complent for SeqData only.
---revcompl' :: SeqData -> SeqData
+
+
+--The following two functions are copied from Ketil Maldes hackage bio package. -- The same functionality has not been reincluded into the biocore package      
+-- | Calculate the reverse complement for SeqData only.
+revcompl' :: SeqData -> SeqData
 revcompl' = L.map compl . L.reverse
 
 -- | Complement a single character.  I.e. identify the nucleotide it 
