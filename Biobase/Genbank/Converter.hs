11a35f36441d520317268c1c9e13e28498eeccaa
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2020-01-09 14:47:19 +0100

    Added commandline options to converter

--- Biobase/Genbank/Converter.hs
+++ Biobase/Genbank/Converter.hs
@@ -1,24 +1,38 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 -- | Parser test script
 --   read from file and directly print parsing output
 
 module Main where
-    
-import System.Environment (getArgs)
-import Biobase.Genbank.Tools
-import Biobase.Genbank.Import
+
+import System.Console.CmdArgs
+import qualified Biobase.Genbank.Import as BGI
 import Biobase.Genbank.Export
-import Biobase.GFF3.Export
+import Biobase.GFF3.Export()
 import Data.Either.Unwrap
+import Paths_Genbank (version) 
+import Data.Version (showVersion)
+
+data Options = Options
+  { inputFilePath :: String,
+    outputFilePath :: String
+  } deriving (Show,Data,Typeable)
+
+options :: Options
+options = Options
+  { inputFilePath = def &= name "i" &= help "Path to input fasta file",
+    outputFilePath = def &= name "o" &= help "Path to output file"
+  } &= summary ("Genbank converter " ++ genbankVersion) &= help "Florian Eggenhofer - 2019-2020" &= verbosity
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let input_file = (head args)
-                                      
-  -- read Clustal outputfile
-  parsedInput <- readGenbank input_file
+  Options{..} <- cmdArgs options
+  parsedInput <- BGI.readGenbank inputFilePath
   if isRight parsedInput
     then do
-      let gffoutput = genbankToGFF3 (fromRight parsedInput)
-      print gffoutput
+      let gffoutput = show $ genbankToGFF3 (fromRight parsedInput)
+      writeFile outputFilePath gffoutput
     else (print (fromLeft parsedInput))
+
+genbankVersion :: String
+genbankVersion = showVersion Paths_Genbank.version
